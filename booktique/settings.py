78c2543f851fab721d64f5d731b689611d807900
genshzin <zahira07nasha@gmail.com>
2024-09-23 15:28:53 +0700

    Implement authentication, session, and cookies

--- booktique/settings.py
+++ booktique/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g^1@g)+yib4&d7o(4$lcljv_p2@ok81!k-9ed^luzt4a7@hp#n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nasha-zahira-booktique.pbp.cs.ui.ac.id"]
 
